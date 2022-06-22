4ec0caadb901aa2391e687243aa0a4d967f11cb5
zy <2280987660@qq.com>
2022-06-22 10:47:01 +0800

    mod: 表结构更新

--- doc/defult.sql
+++ doc/defult.sql
@@ -1,35 +1,32 @@
-# 游戏用户表
-create table t_player
+create table t_base_data
 (
-    userId bigint auto_increment,
-    nickname varchar(32) not null,
-    avatarURL varchar(100) null comment '第三方头像链接',
-    score bigint null comment '积分',
-    isOnline bool null comment '是否在线',
-    offlineTime bigint null comment '离线时间',
-    constraint player_pk
-        primary key (userId)
+    offline_time bigint       null comment '离线时间',
+    is_online    tinyint(1)   null comment '是否在线',
+    score        bigint       null comment '积分',
+    avatar_url   varchar(100) null comment '第三方头像链接',
+    nickname     varchar(32)  not null,
+    user_id      bigint auto_increment
+        primary key
 );
 
-#  账户表
-create table t_account
+create table t_account_data
 (
-    userId int auto_increment,
-    passwd varchar(36) null,
-    equipmentId int null comment '设备ID',
-    account varchar(8) null,
-    constraint t_account_pk
-        primary key (userId)
+    account      varchar(8)  not null,
+    equipment_id varchar(36) not null comment '设备ID',
+    passwd       varchar(36) not null,
+    user_id      int auto_increment
+        primary key
 );
 
 
 create table g_game_data
 (
-    id int auto_increment,
-    userId int null,
     game_data blob null,
-    constraint g_game_data_pk
-        primary key (id)
+    user_id   int  not null
+        primary key
 );
+
+
+
 
 
